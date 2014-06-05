84f25537d7faa2a0c4d79c7e0a0bbaa080299d7e
Ember Baker <erb325@gmail.com>
2014-06-05 13:23:21 -0400

    pull back springs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -14,13 +14,14 @@
     
     CCPhysicsJoint *_catapultJoint;
     CCPhysicsJoint *_pullbackJoint;
+    CCPhysicsJoint *_mouseJoint;
     
     CCNode *_catapultArm;
     CCNode *_catapult;
     CCNode *_levelNode;
     CCNode *_contentNode;
     CCNode *_pullbackNode;
-
+    CCNode *_mouseJointNode;
 }
 
 -(void)didLoadFromCCB {
@@ -36,11 +37,41 @@
     
     _pullbackNode.physicsBody.collisionMask = @[];
     _pullbackJoint = [CCPhysicsJoint connectedSpringJointWithBodyA:_pullbackNode.physicsBody bodyB:_catapultArm.physicsBody anchorA:ccp(0,0) anchorB:ccp(34,138) restLength:60.f stiffness:500.f damping:40.f];
+    
+    _mouseJointNode.physicsBody.collisionMask = @[];
+    
 }
 
 -(void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event{      //called every touch of this scene
     
-    [self lauchPenguin];
+    CGPoint touchLocation = [touch locationInNode:_contentNode];
+    
+    if(CGRectContainsPoint([_catapultArm boundingBox], touchLocation)){
+        _mouseJointNode.position = touchLocation;
+        _mouseJoint = [CCPhysicsJoint connectedSpringJointWithBodyA:_mouseJointNode.physicsBody bodyB:_catapultArm.physicsBody anchorA:ccp(0,0) anchorB:ccp(34,138) restLength:0.f stiffness:3000.f damping:150.f];
+    }
+}
+
+-(void)touchMoved:(UITouch *)touch withEvent:(UIEvent *)event{
+    
+    CGPoint touchLocation = [touch locationInNode:_contentNode];
+    _mouseJointNode.position = touchLocation;
+}
+
+-(void)releaseCatapult{
+    
+    if(_mouseJoint != nil){
+        [_mouseJoint invalidate];
+        _mouseJoint = nil;
+    }
+}
+
+-(void)touchEnded:(UITouch *)touch withEvent:(UIEvent *)event {
+    [self releaseCatapult];
+}
+
+-(void)touchCancelled:(UITouch *)touch withEvent:(UIEvent *)event {
+    [self releaseCatapult];
 }
 
 -(void)lauchPenguin {
