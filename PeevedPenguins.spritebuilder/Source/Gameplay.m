cb38c42baed90300445b457ecee573df798da29f
Ember Baker <erb325@gmail.com>
2014-06-05 11:46:37 -0400

    Penguins being shot
    now the penguins can be shot without movement from catapult yet

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -0,0 +1,41 @@
+//
+//  Gameplay.m
+//  PeevedPenguins
+//
+//  Created by Ember Baker on 6/5/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Gameplay.h"
+
+@implementation Gameplay {
+    
+    CCPhysicsNode *_physicsNode;
+    
+    CCNode *_catapultArm;
+}
+
+-(void)didLoadFromCCB {
+    
+    self.userInteractionEnabled =TRUE;                              //tell this scene to accept touch
+
+}
+
+-(void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event{      //called every touch of this scene
+    
+    [self lauchPenguin];
+}
+
+-(void)lauchPenguin {
+    
+    CCNode* penguin = [CCBReader load:@"Penguin"];
+    penguin.position = ccpAdd(_catapultArm.position, ccp(16,50));
+    
+    [_physicsNode addChild:penguin];
+    
+    CGPoint launchDirection = ccp(1,0);
+    CGPoint force = ccpMult(launchDirection, 8000);
+    [penguin.physicsBody applyForce:force];
+}
+
+@end
